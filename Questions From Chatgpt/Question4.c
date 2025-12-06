c29f1095f297ff6218fb8ac318642eaedee18263
Creativebivek <developed.creat@gmail.com>
2025-12-07 02:12:15 +0545

    Added Answer of Provided Question

--- Questions From Chatgpt/Question4.c
+++ Questions From Chatgpt/Question4.c
@@ -2,3 +2,14 @@
 Write a C program to print numbers from 1 to 100 using any loop (for, while, or do-while).
 */
 
+#include<stdio.h>
+	int main()
+	{
+		int i;
+		// Using For Loop
+		printf("Using For Loop\n");
+		for( i=1; i<=100; i++){ //For Loop
+			printf("%d\n",i);
+			}
+            return 0;
+        }
