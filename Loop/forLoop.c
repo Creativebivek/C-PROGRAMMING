5ca1ccf9dd2025d0bf6d32514cb3fa39489f9bbd
BIVEK MAHATO <developed.creat@gmail.com>
2025-12-07 10:17:32 +0545

    initial commit

---
+++ Loop/forLoop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    int i;
+    printf("Printing numbers from 1 to 10 using for loop:\n");
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
